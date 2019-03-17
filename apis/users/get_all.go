871067387d8c6eeb0f1db19ee07b5165426be303
ervitis <vitomarti@gmail.com>
2019-03-17 20:44:17 +0100

    feat: add paging

--- apis/users/get_all.go
+++ apis/users/get_all.go
@@ -4,10 +4,13 @@ import (
 	"encoding/json"
 	"github.com/ervitis/golang-testing/helpers"
 	"net/http"
+	"strconv"
 )
 
 func (h *ReqHandler) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 	var users []*User
+	const itemsPerPage = 15
+	var page int
 
 	b, err := h.Reader.ReadData("data/users.json")
 	if err != nil {
@@ -17,5 +20,28 @@ func (h *ReqHandler) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 
 	_ = json.Unmarshal(b, &users)
 
-	helpers.Response(w, http.StatusOK, users)
+	qpage := r.FormValue("page")
+	if qpage == "" {
+		page = 1
+	} else {
+		if page, err = strconv.Atoi(qpage); err != nil {
+			helpers.ResponseWithError(w, http.StatusBadRequest, "page is not a number")
+			return
+		}
+	}
+
+	start := (page - 1) * itemsPerPage
+	stop := start + itemsPerPage
+
+	if start > len(users) {
+		helpers.Response(w, http.StatusOK, []*User{})
+		return
+	}
+
+	if stop > len(users) {
+			stop = len(users)
+	}
+
+
+	helpers.Response(w, http.StatusOK, users[start:stop])
 }
